9a1b049588e12240c4e5352801bec35cbf5832d0
20161104595 <a20161104595@mac26.local>
2017-06-12 11:28:03 +0800

    sort
    paid

--- sort/main.c
+++ sort/main.c
@@ -1,15 +1,27 @@
-//
-//  main.c
-//  sort
-//
-//  Created by 20161104595 on 17/6/12.
-//  Copyright © 2017年 20161104595. All rights reserved.
-//
-
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
+#include<stdio.h>
+int main()
+{
+    int i,a[10],j,t;
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(j=0;j<9;j++)
+    {
+    for(i=0;i<=8-j;i++)
+    {
+     if(a[i]>a[i+1])
+     {
+         t=a[i];
+         a[i]=a[i+1];
+         a[i+1]=t;
+     }
+    }
+    }
+        for(i=0;i<10;i++)
+        {
+            printf("%d ",a[i]);
+        }
+    printf("\n");
+        return 0;
+    }
